8ecaf0b06f6c681d9e494d07e9ec7807f90dd093
Steven M <sman2@andrew.cmu.edu>
2025-08-26 02:09:38 -0400

    creating offline traj stats script

--- examples/baselines/dreamerv3-torch/local_config.py
+++ examples/baselines/dreamerv3-torch/local_config.py
@@ -2,19 +2,8 @@ from dataclasses import dataclass
 
 @dataclass
 class LocalArgs:
-    # wm_directory: str = "/home/clown2/Desktop/Work/Research/ManiSkill/Maniskill/ckpt_data/aug24_tempckpt/wm_ckpt.pt"
-    # filter_directory_nogp: str = ''
-    # filter_directory_gp: str = '/home/clown2/Desktop/Work/Research/ManiSkill/Maniskill/ckpt_data/aug24_tempckpt/gp_policy.pth'
-    # offline_data_path: str = '/home/kensuke/ManiSkill/examples/baselines/ppo/runs/BlockTopple-v0__ppo_rgb__1__1753308792/test_videos/trajectory.rgb.pd_ee_delta_pose.physx_cuda.h5'
-
-    # wm_directory: str = "/home/clown2/Desktop/Work/Research/ManiSkill/Maniskill/examples/baselines/dreamerv3-torch/runs/BlockTopple-v0__dreamer__1__1756605634/latest.pt"
-    # filter_directory_nogp: str = ''
-    # filter_directory_gp: str = '/home/clown2/Desktop/Work/Research/ManiSkill/Maniskill/examples/baselines/dreamerv3-torch/runs/LCRL/gp/noise_0.1_actor_lr_0.0001_critic_lr_0.001_batch_512_step_per_epoch_40000_kwargs_{}_seed_1/epoch_id_3/policy.pth'
-    # offline_data_path: str = '/home/clown2/Desktop/Work/Research/ManiSkill/Maniskill/examples/baselines/ppo/runs/BlockTopple-v0__ppo_rgb__1__1756574937/test_videos/trajectory.rgb.pd_ee_delta_pose.physx_cuda.h5'
-
-
-    wm_directory: str = "/home/wmcbf/ManiSkill/examples/baselines/dreamerv3-torch/wm_cbf_checkpoint/wm_lz_sept4.pt"
-    filter_directory_nogp: str = '/home/wmcbf/ManiSkill/examples/baselines/dreamerv3-torch/wm_cbf_checkpoint/no_gp_aug6.pth'
-    filter_directory_gp: str = '/home/wmcbf/ManiSkill/examples/baselines/dreamerv3-torch/wm_cbf_checkpoint/gp_sept4.pth'
-    offline_data_path: str = '/home/kensuke/ManiSkill/examples/baselines/ppo/runs/BlockTopple-v0__ppo_rgb__1__1753308792/test_videos/trajectory.rgb.pd_ee_delta_pose.physx_cuda.h5'
+    wm_directory: str = "/home/clown2/Desktop/Work/Research/ManiSkill/Maniskill/ckpt_data/aug24_tempckpt/wm_ckpt.pt"
+    filter_directory_nogp: str = ''
+    filter_directory_gp: str = '/home/clown2/Desktop/Work/Research/ManiSkill/Maniskill/ckpt_data/aug24_tempckpt/gp_policy.pth'
+    
     
